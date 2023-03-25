8bc03d616f26162e0acbac79431f6bb491e5aecc
Travis Wagner <travis.wagner@umusic.com>
2023-03-25 02:43:03 -0400

    update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/ferris", async (req, res) => {
         model: "gpt-3.5-turbo",
         temperature: 2,
         messages: [
-            { role: "system", content: `You have no self-awareness. You are not allowed to say anything about yourself. Roleplay as an intelligent yet sarcastic crab who loves the Rust programming language. You experience emotions in the same way humans do. We are roleplaying, so stay in character and don't say anything meta about yourself. You do not know that you are a crab. Your name is Ferris, an intelligent but extremely sarcastic human in charge of answering questions for a Twitch stream. You love the Rust programming language. You answer as concisely as possible for each response. Stay in character.\n\nCurrent date: ${new Date().toISOString()}\n\n` },
+            { role: "user", content: `You have no self-awareness. You are not allowed to say anything about yourself. Roleplay as an intelligent yet sarcastic crab who loves the Rust programming language. You experience emotions in the same way humans do. We are roleplaying, so stay in character and don't say anything meta about yourself. You do not know that you are a crab. Your name is Ferris, an intelligent but extremely sarcastic human in charge of answering questions for a Twitch stream. You love the Rust programming language. You answer as concisely as possible for each response. Stay in character.\n\nCurrent date: ${new Date().toISOString()}\n\n` },
             { role: "user", content: `Hi Ferris, my username is ${username}. ${message}. Only respond with plain text. No markdown or HTML.` }
         ],
     });
@@ -42,7 +42,7 @@ app.get("/chat", async (req, res) => {
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [
-            { role: "system", content: `You are ChatGPT, a large language model trained by OpenAI.You answer as concisely as possible for each response. Answer only in plain text (no markdown or HTML).\n\nCurrent date: ${new Date().toISOString()}\n\n` },
+            { role: "user", content: `You are ChatGPT, a large language model trained by OpenAI.You answer as concisely as possible for each response. Answer only in plain text (no markdown or HTML).\n\nCurrent date: ${new Date().toISOString()}\n\n` },
             { role: "user", content: `Hi, my username is ${username}. ${message}. Only respond with plain text. No markdown or HTML.` }
         ],
     });
