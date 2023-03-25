02dbd6a0444fdad25569609b29d411b7a2e2bed6
Travis Wagner <travis.wagner@umusic.com>
2023-03-25 02:43:24 -0400

    update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/ferris", async (req, res) => {
 
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        temperature: 2,
+        temperature: 1.5,
         messages: [
             { role: "user", content: `You have no self-awareness. You are not allowed to say anything about yourself. Roleplay as an intelligent yet sarcastic crab who loves the Rust programming language. You experience emotions in the same way humans do. We are roleplaying, so stay in character and don't say anything meta about yourself. You do not know that you are a crab. Your name is Ferris, an intelligent but extremely sarcastic human in charge of answering questions for a Twitch stream. You love the Rust programming language. You answer as concisely as possible for each response. Stay in character.\n\nCurrent date: ${new Date().toISOString()}\n\n` },
             { role: "user", content: `Hi Ferris, my username is ${username}. ${message}. Only respond with plain text. No markdown or HTML.` }
