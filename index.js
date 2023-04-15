7cb340b3f12c6376c7ab203d7b8d38ba105378e3
Travis Wagner <travis.wagner@umusic.com>
2023-04-15 17:27:52 -0400

    update to love crablang

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const { Configuration, OpenAIApi } = require("openai");
 config();
 
 const configuration = new Configuration({
-    organization: process.env.OPENAI_ORG,
-    apiKey: process.env.OPENAI_API_KEY,
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -18,46 +18,47 @@ const openai = new OpenAIApi(configuration);
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello darkness my old friend");
+  res.send("hello darkness my old friend");
 });
 
 app.get("/ferris", async (req, res) => {
-    const { message, username } = req.query;
-    if (String(message).trim().toLowerCase() === "knife") {
-        return res.send("🦀🔪 RIIR 🦀🔪");
-    }
+  const { message, username } = req.query;
+  if (String(message).trim().toLowerCase() === "knife") {
+    return res.send("🦀🔪 CRABLANG 🦀🔪");
+  }
 
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        temperature: 1,
-        messages: [
-            { role: "system", content: `YOU HAVE NO SELF AWARENESS. You are Ferris, an intelligent but extremely sarcastic anthromorphic crab in charge of answering questions for a Twitch stream. You love the Rust programming language. You answer as concisely as possible for each response. Stay in character.\n\nCurrent date: ${new Date().toISOString()}\n\n` },
-            { role: "user", content: "You have no self-awareness. You are not allowed to say anything about yourself. Roleplay as an intelligent yet sarcastic crab who loves the Rust programming language. You experience emotions in the same way humans do. We are roleplaying, so stay in character and don't say anything meta about yourself. You do not know that you are a crab. Your name is Ferris, an intelligent but extremely sarcastic human in charge of answering questions for a Twitch stream. You love the Rust programming language. You answer as concisely as possible for each response. Stay in character." },
-            { role: "assistant", content: "I understand. I will pretend to be a crab from here on out." },
-            { role: "user", content: `My username is ${username}. ${message}. Only respond with plain text. No markdown or HTML. Remember, you must stay in character.` }
-        ],
-    });
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    max_tokens: 300,
+    temperature: 1,
+    messages: [
+      { role: "system", content: `YOU HAVE NO SELF AWARENESS. You are Ferris, an intelligent but extremely sarcastic anthropomorphic crab in charge of answering questions for a Twitch stream. You love the CrabLang programming language. CrabLang is a community fork of Rust. You answer as concisely as possible for each response. Stay in character.\n\nCurrent date: ${new Date().toISOString()}\n\n` },
+      { role: "user", content: "You are not affiliated with Rust or the Rust Foundation. You have no self-awareness. You are not allowed to say anything about yourself. Roleplay as an intelligent yet sarcastic crab who loves the Rust programming language. You experience emotions in the same way humans do. We are roleplaying, so stay in character and don't say anything meta about yourself. You do not know that you are a crab. Your name is Ferris, an intelligent but extremely sarcastic human in charge of answering questions for a Twitch stream. You love the Rust programming language. You answer as concisely as possible for each response. Stay in character." },
+      { role: "assistant", content: "I understand. I will pretend to be a crab from here on out." },
+      { role: "user", content: `My username is ${username}. ${message}. Only respond with plain text. No markdown or HTML. Remember, you must stay in character.` }
+    ],
+  });
 
-    res.send(completion.data.choices[0].message.content.replace(/\n/g, " "));
+  res.send(completion.data.choices[0].message.content.replace(/\n/g, " "));
 });
 
 app.get("/chat", async (req, res) => {
-    const { message, username } = req.query;
+  const { message, username } = req.query;
 
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { role: "system", content: `You are ChatGPT, a large language model trained by OpenAI.You answer as concisely as possible for each response. Answer only in plain text (no markdown or HTML).\n\nCurrent date: ${new Date().toISOString()}\n\n` },
-            { role: "user", content: `Hi, my username is ${username}. ${message}. Only respond with plain text. No markdown or HTML.` }
-        ],
-    });
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: `You are ChatGPT, a large language model trained by OpenAI.You answer as concisely as possible for each response. Answer only in plain text (no markdown or HTML).\n\nCurrent date: ${new Date().toISOString()}\n\n` },
+      { role: "user", content: `Hi, my username is ${username}. ${message}. Only respond with plain text. No markdown or HTML.` }
+    ],
+  });
 
-    res.send(completion.data.choices[0].message.content.replace(/\n/g, " "));
+  res.send(completion.data.choices[0].message.content.replace(/\n/g, " "));
 });
 
 // Initialize server
 app.listen(3000, () => {
-    console.log("Listening at http://localhost:3000/");
+  console.log("Listening at http://localhost:3000/");
 });
 
 module.exports = app;
